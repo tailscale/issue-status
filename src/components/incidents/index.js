f3cd45721a8f2540515c8afa52b0338869fc4a9c
Andrew Dunham <andrew@du.nham.ca>
2022-06-23 11:50:07 -0400

    incidents: only show incidents newer than 90 days

--- src/components/incidents/index.js
+++ src/components/incidents/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Incident from "./incident";
 import Skeleton from "./skeleton";
 import useDidMount from "../useDidMount";
+import moment from "moment";
 
 const Container = styled.div`
   margin: 32px auto 0 auto;
@@ -19,15 +20,29 @@ const NoFound = styled.div`
   margin: 0 8px;
 `;
 
+// Maximum number of days to show
+const incidentsVisibilityDays = process.env.REACT_APP_INCIDENTS_VISIBILITY_DAYS ?
+  +process.env.REACT_APP_INCIDENTS_VISIBILITY_DAYS : 30;
+
 export default ({ loading, incidents }) => {
   const [hasMounted] = useDidMount();
 
+  // Filter incidents
+  const now = moment();
+  //const now = moment([2025, 0 /* 0-indexed */, 15]);
+  const filtered = incidents?.filter(incident => {
+    const updatedAt = moment(incident.updated_at);
+    const diff = now.diff(updatedAt, "days");
+
+    return diff < incidentsVisibilityDays;
+  });
+
   return (
     <Container>
-      <Title>Incidents</Title>
+      <Title title={`Incidents from last ${incidentsVisibilityDays} days`}>Incidents</Title>
       {!loading || hasMounted ? (
-        incidents?.length > 0 ? (
-          incidents?.map((incident) => (
+        filtered?.length > 0 ? (
+          filtered?.map((incident) => (
             <Incident key={incident.id} incident={incident} />
           ))
         ) : (
