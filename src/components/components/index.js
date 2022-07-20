5c5ede373bb7705ce4ce1f771a4a6e16ddecdb0e
Andrew Dunham <andrew@du.nham.ca>
2022-07-20 15:18:26 -0400

    components: sort list by GitHub issue title

--- src/components/components/index.js
+++ src/components/components/index.js
@@ -6,9 +6,20 @@ import useDidMount from "../useDidMount";
 export default ({ loading, components }) => {
   const [hasMounted] = useDidMount();
 
+  // Copy and sort by the GitHub issue title
+  const sorted = [...components];
+  sorted.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    } else if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+
   return !loading || hasMounted ? (
-    components?.length > 0 ? (
-      components?.map((component) => (
+    sorted?.length > 0 ? (
+      sorted?.map((component) => (
         <Component key={component.id} component={component} />
       ))
     ) : (
